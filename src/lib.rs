fa021ed22792bdcfe98101d1ffdbaff1191f9e26
Adam Bonislavský <adam.bonislav@fedora.email>
2025-04-12 22:18:38 +0200

    It fucking works

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,57 @@
 use fixedbitset::FixedBitSet;
+use pyo3::exceptions::{PyKeyError, PyValueError};
 use pyo3::prelude::*;
 use rapidhash::RapidInlineHasher;
-use statrs::distribution::{ContinuousCDF, Normal};
 use std::collections::HashMap;
 use std::hash::{Hash, Hasher};
 
-fn calc_num_fragments(seq_length: usize, fragment_length: usize, overlap: usize) -> usize {
-    (seq_length / (fragment_length - overlap)) + (seq_length % (fragment_length - overlap) == 0) as usize
+const GOLDEN_RATIO_64: u64 = 0x9e3779b97f4a7c15;
+
+fn det_minimizer<'a>(
+    window: &'a [u8],
+    w: usize,
+    k: usize,
+    h_fn: &mut RapidInlineHasher,
+    previous_minimizer_record: (&'a [u8], usize, usize),
+) -> (&'a [u8], usize, usize) {
+    let (mut prev_minimizer, mut prev_value, mut lifetime) = previous_minimizer_record;
+    let mut lower_bound = 0;
+    if lifetime > 0 {
+        lifetime -= 1;
+        lower_bound = w - 1;
+    } else {
+        prev_value = usize::MAX;
+    }
+
+    for i in lower_bound..w {
+        let end_position = (i + k).min(window.len());
+        let k_mer = &window[i..end_position];
+
+        k_mer.hash(h_fn);
+        let value = (h_fn.finish() ^ GOLDEN_RATIO_64) as usize;
+
+        if value <= prev_value {
+            prev_minimizer = k_mer;
+            prev_value = value;
+            lifetime = i;
+        }
+
+        if end_position == window.len() {
+            break;
+        }
+    }
+
+    (prev_minimizer, prev_value, lifetime)
 }
 
-fn calc_size_per_vector(num_hashes: usize, fragment_length: usize, k: usize) -> usize {
-    let r = 0.01_f64.powf(1.0 / num_hashes as f64);
-    let max_k_mer_num = fragment_length - k + 1;
-    (-1.0 / ((1.0 - r).powf(1.0 / (num_hashes * max_k_mer_num) as f64) - 1.0)).ceil() as usize
-}
-
-fn hash(h_fn_1: &mut RapidInlineHasher, h_fn_2: &mut RapidInlineHasher, hashes: &mut Vec<usize>, fragment: &[u8]) {
+fn hash(
+    h_fn_1: &mut RapidInlineHasher,
+    h_fn_2: &mut RapidInlineHasher,
+    hashes: &mut Vec<usize>,
+    fragment: &[u8],
+) {
     fragment.hash(h_fn_1);
-    let h1 =  h_fn_1.finish();
+    let h1 = h_fn_1.finish();
     hashes.push(h1 as usize);
 
     fragment.hash(h_fn_2);
@@ -29,40 +63,29 @@ fn hash(h_fn_1: &mut RapidInlineHasher, h_fn_2: &mut RapidInlineHasher, hashes: 
     }
 }
 
-pub fn reverse_complement(seq: &[u8]) -> Vec<u8> {
+fn reverse_complement(seq: &[u8]) -> Vec<u8> {
     seq.iter()
         .rev()
         .map(|c| if c & 2 != 0 { c ^ 4 } else { c ^ 21 })
         .collect()
 }
 
-fn calculate_confidence_intervals(standard_norm_dist: Normal, seq_length: usize, k: usize, error_rate: f64, confidence: f64) -> (usize, usize) {
-    let q = 1.0 - (1.0 - error_rate).powi(k as i32);
-    let num_k_mers = seq_length - k + 1;
-    let variance = num_k_mers as f64 * (1.0 - q) * (q * (2.0 * k as f64 + (2.0 / error_rate) - 1.0) - 2.0 * k as f64)
-        + (k * (k - 1)) as f64 * (1.0 - q).powi(2)
-        + ((2.0 * (1.0 - q)) / error_rate.powi(2)) * ((1.0 + (k - 1) as f64 * (1.0 - q)) * error_rate - q);
-    let alpha = 1.0 - confidence;
-    let z = standard_norm_dist.inverse_cdf(1.0 - alpha / 2.0);
-    let mean = num_k_mers as f64 * q;
-    ((mean - z * variance.sqrt()).floor() as usize, (mean + z * variance.sqrt()).ceil() as usize)
-}
-
 #[pyclass]
 struct InterleavedBloomFilter {
     fragment_length: usize,
     overlap: usize,
+    w: usize,
     k: usize,
-    num_vectors: usize,
-    size_per_vector: usize,
-    error_rate: f64,
-    confidence: f64,
+    num_of_bins: usize,
+    single_filter_size: usize,
+    preserved_pct: f64,
+    max_idx: usize,
+    bin_idxs: HashMap<String, usize>,
     active_filter: Vec<FixedBitSet>,
     filters: HashMap<(String, String), Vec<FixedBitSet>>,
     h_fn_1: RapidInlineHasher,
     h_fn_2: RapidInlineHasher,
     hashes: Vec<usize>,
-    standard_norm_dist: Normal,
     counting_vector: Vec<usize>,
     binning_vector: FixedBitSet,
 }
@@ -70,72 +93,122 @@ struct InterleavedBloomFilter {
 #[pymethods]
 impl InterleavedBloomFilter {
     #[new]
-    fn new(max_seq_length: usize,
-           fragment_length: usize,
-           overlap: usize,
-           k: usize,
-           num_hashes: usize,
-           error_rate: f64,
-           confidence: f64) -> Self {
-        let num_vectors = calc_num_fragments(max_seq_length, fragment_length, overlap);
-        let size_per_vector = calc_size_per_vector(num_hashes, fragment_length, k);
-
-        let active_filter: Vec<FixedBitSet> = (0..num_vectors)
+    fn new(
+        num_of_bins: usize,
+        single_filter_size: usize,
+        fragment_length: usize,
+        overlap: usize,
+        w: usize,
+        k: usize,
+        num_hashes: usize,
+        preserved_pct: f64
+    ) -> PyResult<Self> {
+        if num_hashes < 2 {
+            return Err(PyValueError::new_err(
+                "Atleast two hash functions have to be used",
+            ));
+        }
+        if overlap >= fragment_length {
+            return Err(PyValueError::new_err(
+                "Overlap cannot be greater or equal to the fragment length",
+            ));
+        }
+        if preserved_pct <= 0.0 || preserved_pct >= 1.0 {
+            return Err(PyValueError::new_err(
+                "Preserved percentage must be a probability between 0 and 1 (exclusive)",
+            ));
+        }
+        // There is are no single bloom filters...
+        // But the overall interleaved bloom filter size is: <single_filter_size> * <num_of_bins>
+        let max_idx = 0;
+        let active_filter: Vec<FixedBitSet> = (0..single_filter_size)
             .map(|_| {
-                let mut bit_set = FixedBitSet::with_capacity(size_per_vector);
+                let mut bit_set = FixedBitSet::with_capacity(num_of_bins);
                 bit_set.set_range(.., false);
                 bit_set
             })
             .collect();
         let filters: HashMap<(String, String), Vec<FixedBitSet>> = HashMap::new();
+        let bin_idxs: HashMap<String, usize> = HashMap::new();
 
         let hashes: Vec<usize> = Vec::with_capacity(num_hashes);
         let h_fn_1 = RapidInlineHasher::default();
-        let h_fn_2 = RapidInlineHasher::new(0x9e3779b97f4a7c15);
+        let h_fn_2 = RapidInlineHasher::new(GOLDEN_RATIO_64);
 
-        let standard_norm_dist: Normal = Normal::new(0.0, 1.0).unwrap();
-        let counting_vector: Vec<usize> = vec![0; size_per_vector];
-        let binning_vector = FixedBitSet::with_capacity(size_per_vector);
+        let counting_vector: Vec<usize> = vec![0; num_of_bins];
+        let binning_vector = FixedBitSet::with_capacity(num_of_bins);
 
-        Self {
+        Ok(Self {
             fragment_length,
             overlap,
+            w,
             k,
-            error_rate,
-            confidence,
-            num_vectors,
-            size_per_vector,
+            num_of_bins,
+            single_filter_size,
+            max_idx,
+            bin_idxs,
             active_filter,
+            preserved_pct,
             filters,
             h_fn_1,
             h_fn_2,
             hashes,
-            standard_norm_dist,
             counting_vector,
-            binning_vector
-        }
+            binning_vector,
+        })
     }
     pub fn insert_sequence(&mut self, seq_id: (String, String), seq: &str) {
         if self.filters.contains_key(&seq_id) {
             return;
         }
-        // NOTE: duplication
-        let mut filter: Vec<FixedBitSet> = (0..self.num_vectors)
+        let bin_idx = self.bin_idxs.entry(seq_id.0.clone()).or_insert(self.max_idx);
+        *bin_idx += 1;
+        
+        let mut filter: Vec<FixedBitSet> = (0..self.single_filter_size)
             .map(|_| {
-                let mut bit_set = FixedBitSet::with_capacity(self.size_per_vector);
+                let mut bit_set = FixedBitSet::with_capacity(self.num_of_bins);
                 bit_set.set_range(.., false);
                 bit_set
             })
             .collect();
 
-        for (i, frag_start) in (0..seq.len()).step_by(self.fragment_length - self.overlap).enumerate() {
+        for frag_start in (0..seq.len())
+            .step_by(self.fragment_length - self.overlap)
+        {
             let fragment = &seq[frag_start..(frag_start + self.fragment_length).min(seq.len())];
-            let num_k_mers = if fragment.len() >= self.k { fragment.len() - self.k } else { 0 } + 1;
+            let num_windows = if fragment.len() >= (self.w + self.k - 1) {
+                fragment.len() - (self.w + self.k - 1)
+            } else {
+                0
+            } + 1;
 
-            for j in 0..num_k_mers {
-                hash(&mut self.h_fn_1, &mut self.h_fn_2, &mut self.hashes, fragment[j..(j + self.k).min(fragment.len())].as_bytes());
-                for h in self.hashes.drain(..) {
-                    FixedBitSet::set(&mut filter[h % self.num_vectors], i, true);
+            let mut prev_minimizer: &[u8] = &[];
+            let mut prev_value: usize = usize::MAX;
+            let mut lifetime: usize = 0;
+
+            for j in 0..num_windows {
+                let window = fragment[j..(j + self.w + self.k - 1).min(fragment.len())].as_bytes();
+                let (minimizer, value, new_lifetime) = det_minimizer(
+                    window,
+                    self.w,
+                    self.k,
+                    &mut self.h_fn_1,
+                    (prev_minimizer, prev_value, lifetime),
+                );
+
+                if lifetime > 0 && prev_value == value && prev_minimizer == minimizer {
+                    lifetime = new_lifetime;
+                    continue;
+                }
+                prev_minimizer = minimizer;
+                prev_value = value;
+                lifetime = new_lifetime;
+
+                for mer in [prev_minimizer, &reverse_complement(prev_minimizer)] {
+                    hash(&mut self.h_fn_1, &mut self.h_fn_2, &mut self.hashes, mer);
+                    for digest in self.hashes.drain(..) {
+                        FixedBitSet::set(&mut filter[digest % self.single_filter_size], *bin_idx % self.num_of_bins, true);
+                    }
                 }
             }
         }
@@ -143,35 +216,59 @@ impl InterleavedBloomFilter {
         self.filters.insert(seq_id, filter);
     }
 
-    pub fn activate_filter(&mut self, seq_id: (String, String)) {
-        if ! self.filters.contains_key(&seq_id) {
-            panic!("Tried to activate a filter that is not present.");
-        }
+    pub fn activate_filter(&mut self, seq_id: (String, String)) -> PyResult<()> {
+        let filters = self.filters.get(&seq_id).ok_or_else(|| {
+            PyKeyError::new_err("Tried to activate a filter that is not present.")
+        })?;
 
-        for (i, filter) in self.filters.get(&seq_id).unwrap().iter().enumerate(){
+        for (i, filter) in filters.iter().enumerate() {
             self.active_filter[i] |= filter;
         }
+        Ok(())
     }
 
     pub fn is_sequence_present(&mut self, seq: &str) -> bool {
-        let (_, upper) = calculate_confidence_intervals(self.standard_norm_dist, seq.len(), self.k, self.error_rate, self.confidence);
-        let threshold = seq.len() - self.k + 1 - upper;
-        self.is_sequence_present_aux(seq.as_bytes(), threshold) || self.is_sequence_present_aux(&reverse_complement(seq.as_bytes()), threshold)
-    }
-}
-
-impl InterleavedBloomFilter {
-    fn is_sequence_present_aux(&mut self, seq: &[u8], threshold: usize) -> bool {
         let mut max_count: usize = 0;
-        let num_k_mers = if seq.len() >= self.k { seq.len() - self.k } else { 0 } + 1;
+        let num_windows = if seq.len() >= (self.w + self.k - 1) {
+            seq.len() - (self.w + self.k - 1)
+        } else {
+            0
+        } + 1;
+
+        let threshold = (num_windows as f64 * self.preserved_pct).ceil() as usize;
         self.counting_vector.fill(0);
 
-        for i in 0..num_k_mers {
-            self.binning_vector.set_range(.., true);
+        let mut prev_minimizer: &[u8] = &[];
+        let mut prev_value: usize = usize::MAX;
+        let mut lifetime: usize = 0;
 
-            hash(&mut self.h_fn_1, &mut self.h_fn_2, &mut self.hashes, &seq[i..(i + self.k).min(seq.len())]);
-            for digest in self.hashes.drain(..) {
-                self.binning_vector &= &self.active_filter[digest % self.num_vectors];
+        for i in 0..num_windows {
+            let window = &seq[i..(i + self.w + self.k - 1).min(seq.len())];
+            let (minimizer, value, new_lifetime) = det_minimizer(
+                window.as_bytes(),
+                self.w,
+                self.k,
+                &mut self.h_fn_1,
+                (prev_minimizer, prev_value, lifetime),
+            );
+
+            if lifetime > 0 && prev_value == value && prev_minimizer == minimizer {
+                lifetime = new_lifetime;
+            } else {
+                prev_minimizer = minimizer;
+                prev_value = value;
+                lifetime = new_lifetime;
+
+                self.binning_vector.set_range(.., true);
+                hash(
+                    &mut self.h_fn_1,
+                    &mut self.h_fn_2,
+                    &mut self.hashes,
+                    prev_minimizer,
+                );
+                for digest in self.hashes.drain(..) {
+                    self.binning_vector &= &self.active_filter[digest % self.single_filter_size];
+                }
             }
 
             for j in self.binning_vector.ones() {
@@ -180,7 +277,7 @@ impl InterleavedBloomFilter {
                 if max_count >= threshold {
                     return true;
                 }
-                if threshold - max_count > num_k_mers - i - 1 {
+                if threshold - max_count > num_windows - i - 1 {
                     return false;
                 }
             }
